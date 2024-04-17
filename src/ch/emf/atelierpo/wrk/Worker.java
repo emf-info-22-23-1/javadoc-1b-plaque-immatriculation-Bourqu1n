4d6ad27eb0ef4f2c2459a1c585012dee43c3f925
Bourqu1n <noam.bourqui@studentfr.ch>
2024-04-17 08:58:47 +0200

    JavaDoc

--- src/ch/emf/atelierpo/wrk/Worker.java
+++ src/ch/emf/atelierpo/wrk/Worker.java
@@ -6,8 +6,34 @@ package ch.emf.atelierpo.wrk;
  */
 public class Worker {
 
-   
-    public boolean controlePlaqueImmatriculation(String canton, String numeroDePlaque) {
+    /**
+     * Vérifie si une plaque d'immatriculation de voiture suisse est
+     * valide en fonction du canton et du numéro de plaque fournis.
+     * Les seuls caractères autorisés sont les caractères alphabétiques de A à Z
+     * (MAJUSCULE) pour le canton et
+     * des chiffres pour le numéro de plaque.
+     * 
+     * Pour que le canton soit valide, il doit contenir au moins un des ces cas :
+     * "AG, AI, AR, BE, BL, BS, FR, GE, GL, GR, JU, LU, NE, NW, OW, SG, SH, SO, SZ,
+     * TG, TI, UR, VD, VS, ZG, ZH"
+     * 
+     * Comportement souhaité :
+     * 
+     * canton numeroPlaque résultat souhaité
+     * "FR"     "12"        true
+     * "VD2333" "33"        false
+     * "Fr"     "12345"     false
+     * "HP"     "13333"     false
+     * 
+     *
+     * @param canton       Le canton de la plaque d'immatriculation, représenté par
+     *                     une chaîne de caractères ne contenant que des lettres de
+     *                     A à Z.
+     * @param numeroPlaque Le numéro de plaque d'immatriculation, représenté par une
+     *                     chaîne de caractères ne contenant que des chiffres.
+     * @return true si la plaque d'immatriculation est valide, false sinon.
+     */
+    public boolean controlePlaqueImmatriculation(String canton, String numeroPlaque) {
         return false;
     }
 
